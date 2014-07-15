c42f84fb14c6985753ba0247f80112d8a84a7e39
mouezapeter <peter.moueza@etu.univ-nantes.fr>
2014-07-15 21:32:47 +0200

    compile if no asm code from 3dkit and with underscores

--- stereo-0.2b-1/widget/3dkit.c
+++ stereo-0.2b-1/widget/3dkit.c
@@ -133,12 +133,14 @@ static inline int muldiv64 (int m1, int m2, int d)
     //	"imul %%dx\n\t"
     //		"idivl %3\n\t"
 
-    //	"=a" (result)
-    //	"a" (m1), "d" (m2), "g" (d)
-    //	"eax", "edx"
+    //:	"=a" (result)
+    //:	"a" (m1), "d" (m2), "g" (d)
+    //:	"eax", "edx"
 __asm__(
 "\n\t"
-
+:	"=r" (result)
+:	"r" (m1), "r" (m2), "r" (d)
+:	"eax", "edx"
 	);
     return result;
 }
